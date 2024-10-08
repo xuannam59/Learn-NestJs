ef01fe51e71b47631c32b73a1e4c647c07739da4
Xuan Nam <leeminhnam2k2@gmail.com>
2024-10-08 15:54:39 +0700

    data validation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,7 +11,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(configService.get<string>("PORT"));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 // Data transfer object => class = { }
 export class CreateUserDto {
+    @IsEmail({},
+        { message: "Email không đúng định dạng", })
+    @IsNotEmpty(
+        { message: "Email không được để trống", }
+    )
     email: string;
+
+    @IsNotEmpty(
+        { message: "Password không được để trống", }
+    )
     password: string;
     fullName: string;
     address: string;
