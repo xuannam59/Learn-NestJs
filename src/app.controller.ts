11b801c4163efcce243ff4056b4336d3eb0467c6
Xuan Nam <leeminhnam2k2@gmail.com>
2024-10-12 11:07:41 +0700

    enable authezation authentication globally

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -26,5 +27,17 @@ export class AppController {
   @Post("/login")
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('profile1')
+  getProfile1(@Request() req) {
+    return req.user;
   }
 }
