e863756c658bb776dcfa29f477ea6e22fe526fbd
Xuan Nam <leeminhnam2k2@gmail.com>
2024-10-14 18:12:30 +0700

    CORS configuration object

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ async function bootstrap() {
 
   const reflector = app.get(Reflector);
   // app.useGlobalGuards(new JwtAuthGuard(reflector));
-
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+  });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
