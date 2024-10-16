53c56842e081b0ffe83149f70fad0ee8a745f127
Xuan Nam <leeminhnam2k2@gmail.com>
2024-10-16 17:47:09 +0700

    delete company

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -32,7 +32,10 @@ export class CompaniesController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesService.remove(+id);
+  remove(
+    @Param('id') id: string,
+    @User() user: IUser
+  ) {
+    return this.companiesService.remove(id, user);
   }
 }
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -46,7 +46,17 @@ export class CompaniesService {
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string, user: IUser) {
+    await this.companyModel.updateOne({
+      _id: id,
+    }, {
+      deletedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+    return this.companyModel.softDelete({
+      _id: id
+    });
   }
 }
