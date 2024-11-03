d4a69e8d51064774ab4d902fa016d6d299e02251
Xuan Nam <leeminhnam2k2@gmail.com>
2024-11-03 21:13:54 +0700

    add middleware helmet

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -40,6 +41,9 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: ["1", "2"]
   });
+
+  // config helmet
+  app.use(helmet());
 
   await app.listen(configService.get<string>("PORT"));
 }
